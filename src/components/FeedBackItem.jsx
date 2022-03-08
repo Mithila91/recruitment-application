146935711bb35f0c005ae82182fa38d69a0005c6
Mithila91 <Mithila91@users.noreply.github.com>
2022-03-08 01:59:07 +0100

    [WIP]removed some styling

--- src/components/FeedBackItem.jsx
+++ src/components/FeedBackItem.jsx
@@ -8,13 +8,18 @@ export default function FeedBackItem({ item, handleDelete }) {
   return (
     <Card reverse={true}>
       <div className="num-display">{item.id}</div>
+
       <button onClick={() => handleDelete(item.id)} className="close">
         <FaTimes color="white" />
       </button>
-      <div className="text-display">{item.name}</div>
-      <div className="text-display">{item.age}</div>
-      <div className="text-display">{item.email}</div>
-      <div className="text-display">{item.address}</div>
+
+      <div className="text-display">
+        <p>name</p> {item.name}
+        <p>age</p> {item.age}
+        <p>email</p> {item.email}
+        <p>address</p>
+        {item.address}
+      </div>
     </Card>
   )
 }
